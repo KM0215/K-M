42269ec735a8413e15e077d042743afd18661be3
MartinVelkov <martivelkov5@gmail.com>
2023-11-01 15:45:51 +0200

    removing html

--- public/index.js
+++ public/index.js
@@ -4,4 +4,4 @@ import ReactDOM from "react-dom";
 import a from "../src/Components/LoginSingup";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<a />, rootElement);
+ReactDOM.render(<a/>, rootElement);
