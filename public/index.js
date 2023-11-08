7f84bbee743dc97553f88a35e6510aad47a25cfd
MartinVelkov <martivelkov5@gmail.com>
2023-11-08 23:19:03 +0200

    10X8 chess

--- public/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import a from "../src/Components/LoginSingup";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<a/>, rootElement);
